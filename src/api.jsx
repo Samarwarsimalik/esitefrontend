070738e44fcaa5570eacadb22202003c83a2358e
samar <samarakhlaqwarsi@gmail.com>
2026-02-16 10:55:33 +0530

    change src

--- src/api.jsx
+++ src/api.jsx
@@ -1,9 +1,12 @@
 const API = "https://esitebackend.onrender.com/api";
 
-
-export const apiFetch = (url, options = {}) =>
-fetch(`${API}${url}`, {
-credentials: "include",
-headers: { "Content-Type": "application/json" },
-...options,
-});
+export const apiFetch = (url, options = {}) => {
+  return fetch(`${API}${url}`, {
+    credentials: "include",
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options.headers || {}),
+    },
+  });
+};
